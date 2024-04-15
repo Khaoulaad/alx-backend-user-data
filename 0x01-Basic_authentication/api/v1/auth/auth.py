4e52431b10c7f612c80cdcacbceb728f3281fc94
Khaoulaad <ouajaa.khaoula@gmail.com>
2024-04-15 23:26:33 +0000

    0

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from typing import List, TypeVar
+from flask import Flask, request
+
+
+class Auth:
+    ''' A Class to manage the API authentication.
+    '''
+
+    def require_auth(
+            self,
+            path: str,
+            excluded_paths: List[str]
+            ) -> bool:
+        ''' Required auth
+        '''
+        return False
+
+    def authorization_header(
+            self,
+            request=None
+            ) -> str:
+        '''Auth header
+        '''
+        if request is None:
+            return None
+
+        return request.headers.get('Authorization')
+
+    def current_user(
+            self,
+            request=None
+            ) -> TypeVar('User'):
+        ''' Current User
+        '''
+        request = Flask(__name__)
+        return None
